c7016b6a3642245f2b2b0a239c7013f7741f3b6c
Zhichong Fang <fzc621@gmail.com>
2018-08-16 23:42:53 -0400

    w/o cond completed!

--- expt.sh
+++ expt.sh
@@ -7,16 +7,22 @@ svm_dir="../svm_rank"
 svm_learn="${svm_dir}/svm_rank_learn"
 svm_classify="${svm_dir}/svm_rank_classify"
 expt_dir="data/set1bin/expt"
+log_dir="${expt_dir}/log"
+res_dir="${expt_dir}/result"
 
 # python -m src.sample_slice "${DATASET_DIR}/set1bin.train.txt" $DATA_DIR
 # python -m src.sim_feat $DATA_DIR $DATA_DIR
 
-for i in 0 1;
-do
-  # $svm_learn -c 3 "${DATA_DIR}/set1bin.slice${i}.txt" "${expt_dir}/rank${i}.dat"
-  # $svm_classify "${DATA_DIR}/set1bin.train.txt" "${expt_dir}/rank${i}.dat" \
-  #     "${expt_dir}/score${i}.dat"
-  python -m src.sim_click -s 5 "${expt_dir}/para.dat" "${DATA_DIR}/set1bin.train.txt" \
-    "${expt_dir}/score${i}.dat" "${DATA_DIR}/set1bin.train.feat.txt" \
-    "${DATA_DIR}/log${i}.txt"
-done
+# for i in 0 1;
+# do
+#   $svm_learn -c 3 "${DATA_DIR}/set1bin.slice${i}.txt" "${expt_dir}/rank${i}.dat"
+#   $svm_classify "${DATA_DIR}/set1bin.train.txt" "${expt_dir}/rank${i}.dat" \
+#       "${expt_dir}/score${i}.dat"
+#   python -m src.sim_click -s 5 "${expt_dir}/para.dat" "${DATA_DIR}/set1bin.train.txt" \
+#     "${expt_dir}/score${i}.dat" "${DATA_DIR}/set1bin.train.feat.txt" \
+#     "${log_dir}/log${i}.txt"
+# done
+
+# === w/o cond ===
+python -m src.model.wo_cond -n 10 "${DATA_DIR}/set1bin.train.feat.txt" \
+  ${log_dir} "${res_dir}/wo_cond.txt"
---
+++ src/model/wo_cond.py
@@ -0,0 +1,140 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import math
+import random
+import timeit
+import argparse
+import numpy as np
+import scipy.optimize as opt
+from ..lib.data_utils import Query, load_log, load_feat
+from ..lib.utils import makedirs
+from collections import defaultdict, Counter
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+            description='propensity estimation w/o condition')
+    parser.add_argument('-n', default=10, type=int,
+        help='number of top positions for which estimates are desired')
+    parser.add_argument('feat_path', help='feature path')
+    parser.add_argument('log_dir', help='click log dir')
+    parser.add_argument('output_path', help='propensity result path')
+    args = parser.parse_args()
+
+    start = timeit.default_timer()
+
+    M = args.n
+    log0_path = os.path.join(args.log_dir, 'log0.txt')
+    log1_path = os.path.join(args.log_dir, 'log1.txt')
+    log0 = load_log(log0_path)
+    log1 = load_log(log1_path)
+
+    S = defaultdict(set)
+    for q0, q1 in zip(log0, log1):
+        assert q0._qid == q1._qid
+        qid = q0._qid
+        docs0 = q0._docs
+        docs1 = q1._docs
+        for rk0, doc0 in enumerate(docs0, start=1):
+            if rk0 > M:
+                break
+            doc_id0, _ = doc0
+            for rk1, doc1 in enumerate(docs1, start=1):
+                if rk1 > M:
+                    break
+                if rk1 == rk0:
+                    continue
+                doc_id1, _ = doc1
+                if doc_id1 == doc_id0:
+                    S[(rk0, rk1)].add((qid, doc_id0))
+                    S[(rk1, rk0)].add((qid, doc_id0))
+                    break
+
+    n0 = len(log0)
+    n1 = len(log1)
+    assert n0 == n1
+    w = Counter()
+    for q in log0:
+        qid = q._qid
+        docs = q._docs
+        for rk, doc in enumerate(docs, start=1):
+            if rk > M:
+                break
+            doc_id, _ = doc
+            w.update({(qid, doc_id, rk):n0})
+    for q in log1:
+        qid = q._qid
+        docs = q._docs
+        for rk, doc in enumerate(docs, start=1):
+            if rk > M:
+                break
+            doc_id, _ = doc
+            w.update({(qid, doc_id, rk):n1})
+
+    c = Counter()
+    not_c = Counter()
+    for q in log0:
+        qid = q._qid
+        docs = q._docs
+        for rk, doc in enumerate(docs, start=1):
+            if rk > M:
+                break
+            doc_id, delta = doc
+            v = delta / w[(qid, doc_id, rk)]
+            v_ = (1 - delta) / w[(qid, doc_id, rk)]
+            for rk_ in range(1, M + 1):
+                if (qid, doc_id) in S[(rk, rk_)]:
+                    c.update({(rk, rk_): v})
+                    not_c.update({(rk, rk_): v_})
+    for q in log1:
+        qid = q._qid
+        docs = q._docs
+        for rk, doc in enumerate(docs, start=1):
+            if rk > M:
+                break
+            doc_id, delta = doc
+            v = delta / w[(qid, doc_id, rk)]
+            v_ = (1 - delta) / w[(qid, doc_id, rk)]
+            for rk_ in range(1, M + 1):
+                if (qid, doc_id) in S[(rk, rk_)]:
+                    c.update({(rk, rk_): v})
+                    not_c.update({(rk, rk_): v_})
+
+    def p_idx(k):
+        return (k - 1) * M + k - 1
+
+    def r_idx(k, k_):
+        assert k != k_
+        if k < k_:
+            return (k - 1) * M + k_ - 1
+        else:
+            return (k_ - 1) * M + k - 1
+
+    def likelihood(x):
+        r = 0
+        for k in range(1, M + 1):
+            for k_ in range(1, M + 1):
+                if k != k_:
+                    r += c[(k, k_)] * math.log(x[p_idx(k)] * x[r_idx(k, k_)])
+                    r += not_c[(k, k_)] * math.log(1 - x[p_idx(k)] * x[r_idx(k, k_)])
+        return -r
+
+    a, b = 1e-4, 1-1e-4
+    x0 = np.array([random.random() * (b - a) + a] * (M * M))
+    bnds = np.array([(a, b)] * (M * M))
+
+    ret = opt.minimize(likelihood, x0, method='L-BFGS-B', bounds=bnds)
+    xm = ret.x
+    prop_ = [str(xm[p_idx(k)] / xm[p_idx(1)]) for k in range(1, M + 1)]
+
+    feat_queries = load_feat(args.feat_path)
+    makedirs(os.path.dirname(args.output_path))
+    with open(args.output_path, 'w') as fout:
+        for query in feat_queries:
+            fout.write('qid:{} {}\n'.format(query._qid, ' '.join(prop_)))
+
+    end = timeit.default_timer()
+    print('Running time: {:.3f}s.'.format(end - start))
